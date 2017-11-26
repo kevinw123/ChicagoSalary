e6d2735751598aeab498e3b45e5d023ff3362f8c
Kevin Wong <kevinwong_895@hotmail.com>
2017-11-25 22:06:17 -0800

    min and max salary

--- main.py
+++ main.py
@@ -18,14 +18,56 @@ def occupationCount():
 
     y_pos = np.arange(len(jobLabels))
     plt.bar(y_pos, jobCount, align='center', alpha=2.0)
-    plt.xticks(y_pos, jobLabels)
+    plt.xticks(y_pos, jobLabels, rotation=90, fontsize = 7)
     plt.ylabel("Number of people")
     plt.xlabel("Job Titles")
     plt.title("Number of people in each job title in Chicago")
     plt.show()
 
+def topSalary():
+    maxSalary = 0.0
+    index = 0
+    salaryList = {}
+    with open("chicago_salaries.csv") as csvfile:
+        employeeRow = csv.reader(csvfile, delimiter=",")
+        for row in employeeRow:
+            salary = row[6][1:]
+            try:
+                salary = float(salary)
+            except:
+                pass
+
+            if type(salary) is float:
+                salaryList[index] = float(salary)
+
+            index += 1
+
+    print(max(salaryList.values()))
+
+def minSalary():
+    maxSalary = 0.0
+    index = 0
+    salaryList = {}
+    with open("chicago_salaries.csv") as csvfile:
+        employeeRow = csv.reader(csvfile, delimiter=",")
+        for row in employeeRow:
+            salary = row[6][1:]
+            try:
+                salary = float(salary)
+            except:
+                pass
+
+            if type(salary) is float:
+                salaryList[index] = float(salary)
+
+            index += 1
+
+    print(min(salaryList.values()))
+
 def main():
-    occupationCount()
+    # occupationCount()
+    # topSalary()
+    minSalary()
 
 if __name__ == "__main__":
     main()
