0e3b5e046ead34eb6da91ec4ba3c3092875f17d1
Kevin Wong <kevinwong_895@hotmail.com>
2017-11-26 19:26:58 -0800

    Adding menu to select

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def topSalary():
 
             index += 1
 
-    print(max(salaryList.values()))
+    print("Maximum salary in Chicago is: " + str(max(salaryList.values())))
 
 def minSalary():
     index = 0
@@ -60,7 +60,31 @@ def minSalary():
 
             index += 1
 
-    print(min(salaryList.values()))
+    print("Minimum salary in Chicago is: " + str(min(salaryList.values())))
+
+def totalFemale():
+    count = 0
+
+    with open("chicago_salaries.csv") as csvfile:
+        employeeRow = csv.reader(csvfile, delimiter=",")
+        for row in employeeRow:
+            if row[3] == "F":
+                count += 1
+
+    print("Total number of females working in this data is: " + str(count))
+    return count
+
+def totalMale():
+    count = 0
+
+    with open("chicago_salaries.csv") as csvfile:
+        employeeRow = csv.reader(csvfile, delimiter=",")
+        for row in employeeRow:
+            if row[3] == "M":
+                count += 1
+
+    print("Total number of males working in this data is: " + str(count))
+    return count
 
 def numFemaleOfficers():
     count = 0
@@ -72,14 +96,40 @@ def numFemaleOfficers():
                 count += 1
 
 
-    print(count)
+    print("Number of Female Officers in Chicago is: " + str(count))
 
 
 def main():
-    # occupationCount()
-    # topSalary()
-    # minSalary()
-    numFemaleOfficers()
+    ans = True
+    while ans:
+        print("""
+        0.Number of People in each Job (Graph)
+        1.Highest Salary
+        2.Min Salary
+        3.Number of Female Policer Officers
+        4.Number of Males
+        5.Number of Females
+        6.Exit/Quit
+        """)
+        ans = input("What would you like to do? ")
+        if ans == "0":
+            occupationCount()
+        elif ans == "1":
+            topSalary()
+        elif ans == "2":
+            minSalary()
+        elif ans == "3":
+            numFemaleOfficers()
+        elif ans == "4":
+            totalMale()
+        elif ans == "5":
+            totalFemale()
+        elif ans == "6":
+            ans = False
+            print("\n Goodbye")
+        elif ans != "":
+            print("\n Not Valid Choice Try again")
+
 
 if __name__ == "__main__":
     main()
